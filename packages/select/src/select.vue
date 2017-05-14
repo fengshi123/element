31bce9ba270d07083a048ee13d89d243a59b4535
fengshi123 <1508228785@qq.com>
2017-05-14 18:34:20 +0800

    Update select.vue

--- packages/select/src/select.vue
+++ packages/select/src/select.vue
@@ -654,7 +654,7 @@
     },
 
     mounted() {
-       alert("good good study");
+       alert("good good study day day up");
       alert('22111');
       if (this.multiple && Array.isArray(this.value) && this.value.length > 0) {
         this.currentPlaceholder = '';
